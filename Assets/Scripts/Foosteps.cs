a1ab31501bd3ae06417b86654d7417a736377a75
Etienne <etiennejvle@gmail.com>
2021-09-28 15:21:24 +0200

    Footsteps

---
+++ Assets/Scripts/Foosteps.cs
@@ -0,0 +1,33 @@
+using UnityEngine;
+
+public class Foosteps : MonoBehaviour {
+    [SerializeField] private GameObject frontRightFoot, frontLeftFoot, backRightFoot, backLeftFoot;
+    [SerializeField] private GameObject footstepPrefab;
+    [SerializeField] private Etienne.Cue cue;
+
+    [ContextMenu("Front Right")]
+    public void PrintFootstepFrontRight() {
+        PrintFootstep(frontRightFoot);
+    }
+    public void PrintFootstepFrontLeft() {
+        PrintFootstep(frontLeftFoot);
+    }
+    public void PrintFootstepBackRight() {
+        PrintFootstep(backRightFoot);
+    }
+    public void PrintFootstepBackLeft() {
+        PrintFootstep(backLeftFoot);
+    }
+
+    private void PrintFootstep(GameObject foot) {
+        Ray ray = new Ray(foot.transform.position, Vector3.down);
+        if(Physics.Raycast(ray, out RaycastHit hit, 10f)) {
+            Vector3 pos = hit.point;
+            pos.y += .01f;
+            GameObject footstep = GameObject.Instantiate(footstepPrefab, pos, footstepPrefab.transform.rotation);
+            footstep.transform.forward = hit.normal;
+
+            Etienne.AudioManager.Play(cue, pos);
+        }
+    }
+}
