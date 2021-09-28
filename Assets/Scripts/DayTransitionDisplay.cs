2a0571e5b9cabc5460a4487fccdec1982c4a93e7
ElieBouillard <75082918+ElieBouillard@users.noreply.github.com>
2021-09-28 09:18:29 +0200

    [ADD] - LeanTween - Both/Input

---
+++ Assets/Scripts/DayTransitionDisplay.cs
@@ -0,0 +1,69 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class DayTransitionDisplay : MonoBehaviour
+{
+    [SerializeField] private CanvasGroup background;
+    [SerializeField] private CanvasGroup currDayText;
+    [SerializeField] private CanvasGroup previousDayText;
+    [SerializeField] private CanvasGroup roleDay;
+
+    private bool canInitDayTransition = true;
+
+    private void Awake()
+    {
+        background.alpha = 0;
+        currDayText.alpha = 0;
+        previousDayText.alpha = 0;
+        roleDay.alpha = 0;
+        roleDay.transform.localPosition = new Vector3(0, -100);
+    }
+    private void Start()
+    {
+        DayTransitionManager.InitDaytTransitionEvent += InitDayTransition;
+    }
+
+    public void InitDayTransition(string newPreviousDayText, string newCurrDayText)
+    {
+        if (!canInitDayTransition) { return; }
+
+        SetDayText(newPreviousDayText, newCurrDayText);
+
+        background.LeanAlpha(1, 2f);
+
+        previousDayText.LeanAlpha(1, 1f).delay = 2f;
+        roleDay.LeanAlpha(1, 3f).delay = 2f;
+
+        previousDayText.transform.LeanMoveLocalX(-1500, 2f).setEaseInOutBack().delay = 3f;
+        previousDayText.LeanAlpha(0, 2f).delay = 3f;
+        roleDay.transform.LeanMoveLocalX(-3000,2f).setEaseInBack().setEaseInOutBack().delay = 3f;
+
+        currDayText.transform.LeanMoveLocalX(0, 2f).setEaseInOutBack().delay = 3f;
+        currDayText.LeanAlpha(1, 3f).delay = 3f;
+
+        currDayText.LeanAlpha(0, 2f).delay = 7f;
+        roleDay.LeanAlpha(0, 2f).delay = 7f;
+        background.LeanAlpha(0, 2f).setOnComplete(ResetDayTransition).delay = 8f;
+    }
+
+    private void ResetDayTransition()
+    {
+        background.alpha = 0;
+        currDayText.alpha = 0;
+        previousDayText.alpha = 0;
+        roleDay.alpha = 0;
+
+        previousDayText.transform.localPosition = new Vector2(0, 0);
+        currDayText.transform.localPosition = new Vector2(1500, 0);
+        roleDay.transform.localPosition = new Vector3(0, -100);
+    }
+
+    public void SetDayText(string newPreviousDayText, string newCurrentDayText)
+    {
+        previousDayText.GetComponent<TMP_Text>().text = newPreviousDayText;
+        currDayText.GetComponent<TMP_Text>().text = newCurrentDayText;
+    }
+}
---
+++ Assets/Scripts/DayTransitionManager.cs
@@ -0,0 +1,48 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DayTransitionManager : MonoBehaviour
+{
+    [SerializeField] private string[] dayTexts = new string[0];
+
+    private int dayIndex = 0;
+
+    public static event Action<string, string> InitDaytTransitionEvent;
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            InitDayTransition();
+        }
+    }
+
+    private void InitDayTransition()
+    {
+        if(dayIndex >= dayTexts.Length) { return; }
+
+        string tempPreviousDayText;
+        string tempCurrDayText;
+        if (dayIndex == 0)
+        {
+            tempPreviousDayText = "";
+            tempCurrDayText = dayTexts[0];
+        }
+        else if (dayIndex == dayTexts.Length)
+        {
+            tempPreviousDayText = dayTexts[dayTexts.Length];
+            tempCurrDayText = "";
+        }
+        else
+        {
+            tempPreviousDayText = dayTexts[dayIndex - 1];
+            tempCurrDayText = dayTexts[dayIndex];
+        }
+
+        InitDaytTransitionEvent.Invoke(tempPreviousDayText, tempCurrDayText);
+
+        dayIndex++;
+    }
+}
