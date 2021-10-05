c9fc6838fcf1bdaf4f32aa80cd7d3503a29e2362
ElieBouillard <75082918+ElieBouillard@users.noreply.github.com>
2021-10-05 16:51:59 +0200

    [ADD] - Anim bear

---
+++ Assets/Scripts/AnimationManager.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimationManager : MonoBehaviour
+{
+    [SerializeField] private Animator m_animator;
+    [SerializeField] private Rigidbody m_rigidbody;
+
+    private void Update()
+    {
+        if (m_animator.GetBool("walk"))
+        {
+            if(m_rigidbody.velocity.magnitude <= 0f)
+            {
+                m_animator.SetBool("walk", false);
+            }
+        }
+        else
+        {
+            if (m_rigidbody.velocity.magnitude > 0f)
+            {
+                m_animator.SetBool("walk", true);
+            }
+        }
+    }
+
+}
